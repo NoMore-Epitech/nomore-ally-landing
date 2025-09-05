4414f709d335198f9821e3df1fc899109ad29715
Vincent <72189942+badidu09@users.noreply.github.com>
2025-09-05 13:44:25 +0200

    +add allowedHosts fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    preview: {
-      allowedHosts: ["app.nomore-app.me"], // ✅ Ajout de l'hôte autorisé
-    },
+  },
+  preview: {
+    allowedHosts: ["app.nomore-app.me"], // ✅ ici c’est bon
   },
   plugins: [
     react(),
